a9a91cbfdf3301169a53123ce18cdb569dfcd47c
nazarmih227 <mihailnazarov6537@gmail.com>
2025-09-10 22:01:09 +0300

    12

---
+++ src/PersonAccount.java
@@ -0,0 +1,11 @@
+class PersonAccount {
+    String name;      // имя и фамилия
+    Double moneyRUB;  // счёт в рублях
+    Double moneyUSD;  // счёт в долларах
+    Double moneyEUR;  // счёт в евро
+
+
+    PersonAccount(String userName) {
+        name = userName;
+    }
+}
